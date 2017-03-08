0489f30e37b2a4fd5ffda53b3da566b979e5e8fe
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-03-08 21:01:53 +0300

    Custom robot controller for android. First working version.

--- ControllerAndrod/app/src/main/java/com/example/mrtrizer/controllerandroid/MainActivity.java
+++ ControllerAndrod/app/src/main/java/com/example/mrtrizer/controllerandroid/MainActivity.java
@@ -3,27 +3,204 @@ package com.example.mrtrizer.controllerandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "bluetooth1";
+
+    // SPP UUID сервиса
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    // MAC-адрес Bluetooth модуля
+    private static String address = "20:16:09:26:72:18";
+
+    Button buttonConnect;
+
+    Button buttonForward;
+    Button buttonStop;
+    Button buttonBack;
+    Button buttonLeft;
+    Button buttonRight;
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        buttonConnect = (Button) findViewById(R.id.button_connect);
+
+        buttonForward = (Button) findViewById(R.id.button_forward);
+        buttonStop = (Button) findViewById(R.id.button_stop);
+        buttonBack = (Button) findViewById(R.id.button_back);
+        buttonLeft = (Button) findViewById(R.id.button_left);
+        buttonRight = (Button) findViewById(R.id.button_right);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
+
+        buttonConnect.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connect();
+            }
+        });
+
+        buttonForward.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                sendData("f");
+            }
+        });
+
+        buttonStop.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                sendData("s");
+            }
+        });
+
+        buttonBack.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                sendData("b");
+            }
+        });
+
+        buttonLeft.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                sendData("l");
+            }
+        });
+
+        buttonRight.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                sendData("r");
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+    public void connect() {
+        // Establish the connection.  This will block until it connects.
+        Log.d(TAG, "...Соединяемся...");
+        try {
+            btSocket.connect();
+            Log.d(TAG, "...Соединение установлено и готово к передачи данных...");
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "...onResume - попытка соединения...");
+
+        // Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+
+        connect();
+
+        // Create a data stream so we can talk to server.
+        Log.d(TAG, "...Создание Socket...");
+
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "...In onPause()...");
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter==null) {
+            errorExit("Fatal Error", "Bluetooth не поддерживается");
+        } else {
+            if (btAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth включен...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void errorExit(String title, String message){
+        Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+
+        Log.d(TAG, "...Посылаем данные: " + message + "...");
+
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+            if (address.equals("00:00:00:00:00:00"))
+                msg = msg + ".\n\nВ переменной address у вас прописан 00:00:00:00:00:00, вам необходимо прописать реальный MAC-адрес Bluetooth модуля";
+            msg = msg +  ".\n\nПроверьте поддержку SPP UUID: " + MY_UUID.toString() + " на Bluetooth модуле, к которому вы подключаетесь.\n\n";
+
+            errorExit("Fatal Error", msg);
+        }
+    }
+
 }
